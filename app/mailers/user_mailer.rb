50a4322fa2183997937f5696986584a43f183c13
Albert Oliva <albert.oliva@gmail.com>
2026-02-08 13:02:21 +0200

    Implement manual root_url construction in UserMailer and views

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,12 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     I18n.with_locale(user.language) do
       @user = user
+      # Manually construct root_url string
+      protocol = default_url_options[:protocol] || 'http'
+      host = default_url_options[:host] || 'localhost'
+      port = default_url_options[:port]
+      @root_url = "#{protocol}://#{host}#{port ? ':' + port.to_s : ''}/"
+
       mail(
         to: @user.email,
         subject: I18n.t('user_mailer.welcome_email.subject')
